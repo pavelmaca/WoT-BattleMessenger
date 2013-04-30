52a352f9e42a1ca6f8cdd6c055b7ec40e1061848
Pavel Máca <maca.pavel@gmail.com>
2013-04-30 13:29:03 +0200

    remove XVM files

--- src/xvm/swf/1.make-patched-swfs.bat
+++ src/xvm/swf/1.make-patched-swfs.bat
@@ -1,10 +1,7 @@
 @echo off
 
 set copy_files=
-set patch_swfs=battleloading.swf PlayersPanel.swf StatisticForm.swf FinalStatistic.swf
-set patch_swfs=%patch_swfs% battle.swf TeamBasesPanel.swf Minimap.swf VehicleMarkersManager.swf
-set patch_swfs=%patch_swfs% TeamMemberRenderer.swf TeamRenderer.swf
-set patch_swfs=%patch_swfs% UserInfo.swf lobby_messenger.swf crew.swf SquadMemberRenderer.swf BattleMessenger.swf
+set patch_swfs=BattleMessenger.swf
 set patch_xmls=
 
 rem Patch XMLs
--- src/xvm/swf/make-diff.bat
+++ src/xvm/swf/make-diff.bat
@@ -1,9 +1,6 @@
 @echo off
 
-set patch_swfs=battleloading.swf PlayersPanel.swf StatisticForm.swf FinalStatistic.swf
-set patch_swfs=%patch_swfs% battle.swf TeamBasesPanel.swf Minimap.swf VehicleMarkersManager.swf
-set patch_swfs=%patch_swfs% TeamMemberRenderer.swf TeamRenderer.swf
-set patch_swfs=%patch_swfs% UserInfo.swf lobby_messenger.swf crew.swf SquadMemberRenderer.swf BattleMessenger.swf
+set patch_swfs=BattleMessenger.swf
 
 for %%i in (%patch_swfs%) do call :do_file %%~ni
 
